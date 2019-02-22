149b95faa7d78299836bca5b14482f02aff08c0d
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2019-02-22 16:39:45 +0200

    add postgres with postgis: 5432, https://hub.docker.com/r/mdillon/postgis

---
+++ postgresql/mdillion/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+APP_NAME=forcelate-postgresql-mdillion
+TAG=10
+POSTGRES_PASSWORD=root
+
+VOLUME_PATH=$(pwd)/volume-postgresql
+
+docker run -d --rm --name ${APP_NAME} \
+    -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+    -v ${VOLUME_PATH}:/var/lib/postgresql/data \
+    -p 5432:5432 \
+    mdillon/postgis:${TAG}
---
+++ postgresql/mdillion/stop.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+APP_NAME=forcelate-postgresql-mdillion
+
+docker stop ${APP_NAME}
