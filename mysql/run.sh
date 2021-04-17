b0622c8607caaa46c7aadf880d225cde0276de3f
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2021-04-17 11:29:32 +0300

    add mysql volume

--- mysql/run.sh
+++ mysql/run.sh
@@ -4,7 +4,10 @@ APP_NAME=tech1-mysql
 TAG=5.7.24
 MYSQL_ROOT_PASSWORD=root
 
+VOLUME_PATH=$(pwd)/volume-mysql
+
 docker run -d --rm --name ${APP_NAME} \
     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+    -v ${VOLUME_PATH}:/var/lib/mysql \
     -p 3306:3306 \
     mysql:${TAG}
